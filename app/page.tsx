c9fc62ecdb7505599d38473ecc8569c9178d97dc
Ken Murata <monpekeeno222@gmail.com>
2024-11-21 22:44:34 +0900

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "../components/App";
 
 export default function Page() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // or a loading spinner, etc.
+  }
+
   return (
     <BrowserRouter>
       <App />
